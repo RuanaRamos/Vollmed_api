f55e7e9e9aa33f16710d25394f49a89c09f325e7
Ruana Ramos <ruanarbarbosa2@icloud.com>
2026-01-10 14:32:45 +0100

    Ein neues Projekt starten

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,33 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+
+  private String logradouro;
+  private String bairro;
+  private String cep;
+  private String uf;
+  private String complemento;
+  private String numero;
+  private String cidade;
+
+
+  public Endereco(DadosEndereco dados) {
+    this.logradouro = dados.logradouro();
+    this.bairro = dados.bairro();
+    this.cep = dados.cep();
+    this.uf = dados.uf();
+    this.numero = dados.numero();
+    this.cidade = dados.cidade();
+    this.complemento = dados.complemento();
+
+  }
+}
