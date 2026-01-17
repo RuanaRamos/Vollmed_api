161b514184e72c5a48ef1c168edfe7cbd42959ef
Ruana Ramos <ruanarbarbosa2@icloud.com>
2026-01-17 22:01:41 +0100

    Behandlung von Fehlern 400 (Bad Request) und 404 (Not Found).

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,7 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,7 +16,19 @@ public class TratadorDeErros{
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity tratarErro400(){
-        return  ResponseEntity.badRequest().build();
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+
+        return  ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
     }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+
+        }
+
+    }
+
 }
