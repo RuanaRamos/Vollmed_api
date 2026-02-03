42e6e20a7c6b20a71b432168ee02e4e7f48d060b
Ruana Ramos <ruanarbarbosa2@icloud.com>
2026-02-03 11:05:23 +0100

    Fehlerkorrektur in der JWT-Konfiguration

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -15,7 +15,7 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    @Value("${api.security.token.secret}")
+    @Value("${api.security.token.secret:${API_SECURITY_TOKEN_SECRET:123456}}")
     private String secret;
 
     public String gerarToken(Usuario usuario) {
